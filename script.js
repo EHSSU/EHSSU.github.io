22a52726bd6c135ae874647f86825f820169428e
TheEdamCheese <79421202+TheEdamCheese@users.noreply.github.com>
2023-04-19 00:51:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function iconHover(id, element) {
+    const select = document.getElementById(id).getElementsByClassName(element)
+    select.style.background="white";
+    select.style.color="blue"
+    document.getElementById
+}
+
+
+function iconLeave(id, element) {
+    const select = document.getElementById(id).querySelector(element)
+    select.style.background="transparent";
+    select.style.color="white";
+}
