87b24cbf56029242ef58f7863938be01e0bc6262
kamicavi <46902924+kamicavi@users.noreply.github.com>
2023-05-22 19:25:40 +0100

    removed unused and broken code for hover stuff

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-function iconHover(id, element) {
-    const select = document.getElementById(id).getElementsByClassName(element)
-    select.style.background="white";
-    select.style.color="blue"
-    document.getElementById
-}
-
-
-function iconLeave(id, element) {
-    const select = document.getElementById(id).querySelector(element)
-    select.style.background="transparent";
-    select.style.color="white";
-}
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* Toggle between adding and removing the "hidden" class to topnav when the user clicks on the icon */
 function nav() {
   var x = document.getElementById("nav-dropdown");
   if (x.className === "hidden") {
